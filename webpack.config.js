f44a79f119e9d22907c57f902cffc90efe0748a4
Alex Gorodov <a.gorodov.22@gmail.com>
2023-01-10 14:51:20 +0200

    esling full installing and setting

--- webpack.config.js
+++ webpack.config.js
@@ -13,27 +13,27 @@ const isProd = !isDev;
 const optimization = () => {
   const config = {
     splitChunks: {
-      chunks: 'all'
-    }
+      chunks: 'all',
+    },
   };
   if (isProd) {
     config.minimizer = [
       new CssMinimizerWebpackPlugin(),
-      new TerserWebpackPlugin()
+      new TerserWebpackPlugin(),
     ];
   }
   return config;
 };
 
-const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`;
+const filename = (ext) => (isDev ? `[name].${ext}` : `[name].[hash].${ext}`);
 
-const cssLoaders = extra => {
+const cssLoaders = (extra) => {
   const loaders = [
     {
-      loader: MiniCssExtractPlugin.loader, 
+      loader: MiniCssExtractPlugin.loader,
       options: {},
     },
-    'css-loader'
+    'css-loader',
   ];
 
   if (extra) {
@@ -43,14 +43,14 @@ const cssLoaders = extra => {
   return loaders;
 };
 
-const babelOptions = preset => {
+const babelOptions = (preset) => {
   const opts = {
     presets: [
-      '@babel/preset-env'
+      '@babel/preset-env',
     ],
     plugins: [
-      '@babel/plugin-proposal-class-properties'
-    ]
+      '@babel/plugin-proposal-class-properties',
+    ],
   };
 
   if (preset) {
@@ -63,7 +63,7 @@ const babelOptions = preset => {
 const jsLoaders = () => {
   const loaders = [{
     loader: 'babel-loader',
-    options: babelOptions()
+    options: babelOptions(),
   }];
 
   if (isDev) {
@@ -80,28 +80,28 @@ const plugins = () => {
       minify: {
         collapseWhitespace: isProd,
       },
-      inject: 'body'
+      inject: 'body',
     }),
     new CleanWebpackPlugin(),
     new CopyWebpackPlugin({
       patterns: [
         {
           from: path.resolve(__dirname, 'src/favicon.ico'),
-          to: path.resolve(__dirname, 'dist')
-        }
-      ]
+          to: path.resolve(__dirname, 'dist'),
+        },
+      ],
     }),
     new MiniCssExtractPlugin({
-      filename: filename('css')
+      filename: filename('css'),
     }),
-  ]
+  ];
 
   if (isProd) {
-    base.push(new BundleAnalizerPlugin())
+    base.push(new BundleAnalizerPlugin());
   }
 
-  return base
-}
+  return base;
+};
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
@@ -109,67 +109,67 @@ module.exports = {
   entry: './index.jsx',
   output: {
     filename: filename('js'),
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   resolve: {
     // Eliminates the need to indicate the extensions of the connected files
     extensions: ['.js', '.css', '.less', '.scss', '.sass', '.ts', '.jsx'],
-    
+
     // Eliminates the need to specify ways to files
     alias: {
       '@styles': path.resolve(__dirname, 'src/styles'),
       '@': path.resolve(__dirname, 'src'),
-    }
+    },
   },
   plugins: plugins(),
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: cssLoaders()
+        use: cssLoaders(),
       },
       {
         test: /\.less$/,
-        use: cssLoaders('less-loader')
+        use: cssLoaders('less-loader'),
       },
       {
         test: /\.(s[ac]ss)$/,
-        use: cssLoaders('sass-loader')
+        use: cssLoaders('sass-loader'),
       },
       {
         test: /\.(png|jpg|svg|webp)$/,
-        type: 'asset/resource'
+        type: 'asset/resource',
       },
       {
         test: /\.(ttf|woff|woff2|eot)$/,
-        type: 'asset/resource'
+        type: 'asset/resource',
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: jsLoaders()
+        use: jsLoaders(),
       },
       {
         test: /\.ts$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          options: babelOptions('@babel/preset-typescript')
-        }
+          options: babelOptions('@babel/preset-typescript'),
+        },
       },
       {
         test: /\.jsx$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          options: babelOptions('@babel/preset-react')
-        }
-      }
-    ]
+          options: babelOptions('@babel/preset-react'),
+        },
+      },
+    ],
   },
   optimization: optimization(),
   devServer: {
     port: 8800,
-    hot: isDev
-  }
+    hot: isDev,
+  },
 };
